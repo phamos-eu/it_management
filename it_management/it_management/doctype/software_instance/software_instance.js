abe2c0c750d37cfb3e814304697b0b61c0041e6d
Raffael Meyer <raffael@alyf.de>
2019-02-18 19:25:53 +0100

    new doctype software instance

---
+++ it_management/it_management/doctype/software_instance/software_instance.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, IT-Geräte und IT-Lösungen wie Server, Rechner, Netzwerke und E-Mailserver sowie auch Backups, and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Software Instance', {
+	refresh: function(frm) {
+
+	}
+});
