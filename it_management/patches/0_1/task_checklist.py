bcb596c29bd39b777f5eff616c70cdb565aba5a9
Raffael Meyer <raffael@alyf.de>
2019-03-21 18:10:20 +0100

    add patches

---
+++ it_management/patches/0_1/task_checklist.py
@@ -0,0 +1,13 @@
+import frappe
+
+"""
+`checklist_table` in `Task` has been renamed to `task_checklist`
+"""
+
+tc_list = frappe.get_list("Task Checklist", filters={'parentfield':'checklist_table'})
+
+for name in tc_list:
+    tl = frappe.get_doc("Task Checklist", name)
+    tl.parentfield = 'task_checklist'
+    tl.save()
+
