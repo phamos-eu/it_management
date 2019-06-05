44fefaac364064094152413b1cd949688d7df462
Raffael Meyer <raffael@alyf.de>
2019-06-05 02:55:24 +0200

    Incident "add activity": comment & timesheet

--- it_management/it_management/doctype/incident/incident.js
+++ it_management/it_management/doctype/incident/incident.js
@@ -2,7 +2,86 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Incident', {
-	refresh: function(frm) {
-
-	}
+	refresh: function (frm) {
+		frm.add_custom_button('Add Activity', function () { frm.trigger('add_activity') });
+	},
+	add_activity: function (frm) {
+		incident_activity_dialog(frm);
+	},
 });
+
+function incident_activity_dialog(frm) {
+	const activity = new frappe.ui.Dialog({
+		title: __('New Activity'),
+		fields: [
+			{
+				fieldtype: 'Datetime',
+				label: __("From Time"),
+				fieldname: 'from_time',
+				default: frappe.datetime.now_datetime()
+			},
+			{
+				fieldtype: 'Column Break',
+				fieldname: 'cb_1',
+			},
+			{
+				fieldtype: 'Float',
+				fieldname: 'hours',
+				label: __("Hours"),
+				default: 0.25
+			},
+			{
+				fieldtype: 'Check',
+				fieldname: 'billable',
+				label: __("Bill"),
+				default: 1
+			},
+			{
+				fieldtype: 'Section Break',
+				fieldname: 'sb_1',
+			},
+			{
+				fieldtype: 'Text Editor',
+				fieldname: 'note',
+			},
+		],
+	})
+
+	activity.set_primary_action(__("Save"), (dialog) => {
+		frm.timeline.insert_comment("Comment", dialog.note);
+
+		let timesheet = {
+			doctype: "Timesheet",
+			note: dialog.note,
+			time_logs: [
+				{
+					from_time: dialog.from_time,
+					hours: dialog.hours,
+					project: frm.project,
+					billable: dialog.billable,
+				}
+			]
+		};
+
+		// Get employee for logged user
+		const options = { user_id: frappe.session.user };
+		const fields = ['name', 'company'];
+
+		frappe.db.get_value('Employee', options, fields)
+			.then(({ message: employee }) => {
+				if (employee) {
+					timesheet["employee"] = employee.name;
+					timesheet["company"] = employee.company;
+				}
+			})
+			.then(() => {
+				frappe.db.insert(timesheet);
+			})
+			.then(() => {
+				activity.hide();
+				activity.clear();
+			});
+	})
+
+	activity.show();
+}
