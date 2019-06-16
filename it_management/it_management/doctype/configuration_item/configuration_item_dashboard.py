cf5741971f8f5471a07eefa621bd2dad7408246c
Raffael Meyer <raffael@alyf.de>
2019-06-16 12:39:01 +0200

    IT Ticket in CI Dashboard

--- it_management/it_management/doctype/configuration_item/configuration_item_dashboard.py
+++ it_management/it_management/doctype/configuration_item/configuration_item_dashboard.py
@@ -4,14 +4,17 @@ from frappe import _
 def get_data():
 	return {
 		'fieldname': 'configuration_item',
+		'non_standard_fieldnames': {
+			'IT Ticket': 'dynamic_name'
+		},
 		'transactions': [
 			{
-				'label': _('Support History'),
-				'items': ['Task']
+				'label': _('Service'),
+				'items': ['IT Ticket', 'Task']
 			},
 			{
 				'label': _('Software'),
 				'items': ['Software Instance', 'User Account', 'Solution']
-			},
+			}
 		]
 	}
