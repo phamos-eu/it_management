2b44ad75008e3b7f93aa597ba9ab48b1fd51b8e3
Raffael Meyer <raffael@alyf.de>
2019-06-16 12:44:05 +0200

    remove solution from CI dashboard

--- it_management/it_management/doctype/configuration_item/configuration_item_dashboard.py
+++ it_management/it_management/doctype/configuration_item/configuration_item_dashboard.py
@@ -11,7 +11,7 @@ def get_data():
 			},
 			{
 				'label': _('Software'),
-				'items': ['Software Instance', 'User Account', 'Solution']
+				'items': ['Software Instance', 'User Account']
 			},
 		]
 	}
