5c4806b5b51c56496ce6ccc303324f44ca8639d7
Raffael Meyer <raffael@alyf.de>
2019-06-16 13:06:16 +0200

    Solution: add IT Ticket to dashboard

--- it_management/it_management/doctype/solution/solution_dashboard.py
+++ it_management/it_management/doctype/solution/solution_dashboard.py
@@ -1,25 +1,29 @@
 from __future__ import unicode_literals
 from frappe import _
 
+
 def get_data():
-	return {
-		'fieldname': 'solution',
-		'transactions': [
-			{
-				'label': _('Support & Projects'),
-				'items': ['Project','Task']
-			},
-      {
-				'label': _('User Accounts and Groups'),
-				'items': ['User Account','User Group']
-			},
-      {
-				'label': _('Software Instances'),
-				'items': ['Software Instance']
-			},
-			{
-				'label': _('Configuration Items & Solutions'),
-				'items': ['Configuration Item','Solution']
-			},
-		]
-}
+    return {
+        'fieldname': 'solution',
+        'non_standard_fieldnames': {
+            'IT Ticket': 'dynamic_name'
+        },
+        'transactions': [
+            {
+                'label': _('Service'),
+                'items': ['IT Ticket', 'Project', 'Task']
+            },
+            {
+                'label': _('User Accounts and Groups'),
+                'items': ['User Account', 'User Group']
+            },
+            {
+                'label': _('Software Instances'),
+                'items': ['Software Instance']
+            },
+            {
+                'label': _('Configuration Items & Solutions'),
+                'items': ['Configuration Item', 'Solution']
+            },
+        ]
+    }
