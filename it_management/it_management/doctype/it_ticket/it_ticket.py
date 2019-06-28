8c645a60bc6d577fb76e27f5c57772c0de5943f8
Raffael Meyer <raffael@alyf.de>
2019-06-28 13:38:18 +0200

    relink emails server-side

--- it_management/it_management/doctype/it_ticket/it_ticket.py
+++ it_management/it_management/doctype/it_ticket/it_ticket.py
@@ -6,8 +6,31 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+
 class ITTicket(Document):
     def onload(self):
         if self.contact:
             # load contact data to be displayed
-            self.set_onload('contact_list', [frappe.get_doc("Contact", self.contact)])
+            self.set_onload("contact_list", [frappe.get_doc("Contact", self.contact)])
+
+
+@frappe.whitelist()
+def relink_email(doctype, name, it_ticket):
+    """Relink Email to IT Ticket.
+
+    params:
+    doctype -- Doctype of the reference document
+    name -- Name of the reference document
+    ticket_name -- Name of the IT Ticket
+    """
+    comm_list = frappe.get_list("Communication", filters={
+        "reference_doctype": doctype,
+        "reference_name": name,
+    })
+
+    for email in comm_list:
+        frappe.email.relink(
+            name=email.name,
+            reference_doctype="IT Ticket",
+            reference_name=it_ticket
+        )
