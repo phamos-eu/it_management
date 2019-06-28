f5cb8266cea15d00442e45620cda4d7a8686a3a1
Raffael Meyer <raffael@alyf.de>
2019-06-28 16:31:51 +0200

    copy comments

--- it_management/it_management/doctype/it_ticket/it_ticket.py
+++ it_management/it_management/doctype/it_ticket/it_ticket.py
@@ -5,13 +5,15 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+import json
 
 
 class ITTicket(Document):
     def onload(self):
         if self.contact:
             # load contact data to be displayed
-            self.set_onload("contact_list", [frappe.get_doc("Contact", self.contact)])
+            self.set_onload("contact_list", [
+                            frappe.get_doc("Contact", self.contact)])
 
     def before_insert(self):
         if self.project and not self.customer:
@@ -21,7 +23,7 @@ class ITTicket(Document):
 
 @frappe.whitelist()
 def relink_email(doctype, name, it_ticket):
-    """Relink Email to IT Ticket.
+    """Relink Email and copy comments to IT Ticket.
 
     params:
     doctype -- Doctype of the reference document
@@ -39,3 +41,11 @@ def relink_email(doctype, name, it_ticket):
             reference_doctype="IT Ticket",
             reference_name=it_ticket
         )
+
+    # copy comments
+    doc = frappe.get_doc(doctype, name)
+    ticket = frappe.get_doc("IT Ticket", it_ticket)
+
+    if doc._comments:
+        for comment in json.loads(doc._comments):
+            ticket.add_comment(comment.comment)
