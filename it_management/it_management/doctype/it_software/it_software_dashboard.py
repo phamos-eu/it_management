babe7eacc10632aa30e3169d5e240799b2982674
Wolfram Schmidt <wolfram.schmidt@tueit.de>
2020-02-15 02:31:48 +0100

    Update it_software_dashboard.py

--- it_management/it_management/doctype/it_software/it_software_dashboard.py
+++ it_management/it_management/doctype/it_software/it_software_dashboard.py
@@ -1,14 +1,10 @@
-"""
-"""
 from __future__ import unicode_literals
 from frappe import _
 
 
 def get_data():
-    """
-    """
     return {
-        'fieldname': 'it_software',
+        'fieldname': 'it software',
         'non_standard_fieldnames': {
             #'IT Ticket': 'dynamic_name'
 			'Issue': 'dynamic_name'
