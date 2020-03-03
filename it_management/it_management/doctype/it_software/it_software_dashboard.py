bc71b20814de6049cb8acbbcf177385f16d4a85f
Joelios <joel.mesmer@libracore.com>
2020-03-03 13:36:53 +0100

    #64

--- it_management/it_management/doctype/it_software/it_software_dashboard.py
+++ it_management/it_management/doctype/it_software/it_software_dashboard.py
@@ -4,15 +4,18 @@ from frappe import _
 
 def get_data():
     return {
-        'fieldname': 'it software',
+        'fieldname': 'it_software',
         'non_standard_fieldnames': {
             #'IT Ticket': 'dynamic_name'
-			'Issue': 'dynamic_name'
+			'Issue': 'dynamic_name',
+			'Software Instance': 'software',
+			'Licence': 'software',
+			'IT Backup': 'software'
         },
         'transactions': [
             {
                 'label': _('Software'),
-                'items': ['Software Instance','Licence']
+                'items': ['Software Instance', 'Licence', 'IT Backup']
             },
         ]
     }
