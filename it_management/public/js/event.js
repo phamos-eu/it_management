2039e7e8bc3ff42114a54a3024dd01f2ea013616
Marius Widmann <marius.widmann@tueit.de>
2020-10-13 12:25:17 +0200

    Closes #123

--- it_management/public/js/event.js
+++ it_management/public/js/event.js
@@ -1,10 +1,12 @@
 cur_frm.dashboard.add_transactions([
-	{
+    /*
+    {
 		'items': [
 			'Trip'
 		],
 		'label': 'Activity'
-	}
+    }
+    */
 ]);
 frappe.ui.form.on('Event', {
 	onload: function (frm) {
