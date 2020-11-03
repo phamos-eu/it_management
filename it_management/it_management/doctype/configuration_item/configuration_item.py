5a4dfcf4cf91c117ab5c924868b88450203b09e6
Marius Widmann <marius.widmann@gmail.com>
2020-11-03 16:10:52 +0000

    Closes #151

--- it_management/it_management/doctype/configuration_item/configuration_item.py
+++ it_management/it_management/doctype/configuration_item/configuration_item.py
@@ -7,4 +7,13 @@ import frappe
 from frappe.model.document import Document
 
 class ConfigurationItem(Document):
+	def save(self, *args, **kwargs):
+		if self.status == 'Obsolet':
+			sis = frappe.db.get_all('Software Instance',filters={'configuration_item':self.name}, fields=['name'],page_length=10000,as_list=False)
+			for si in sis:
+				frappe.db.set_value('Software Instance',si["name"],{
+					'status':'Obsolet'
+				})
+
+		super().save(*args, **kwargs)
 	pass
