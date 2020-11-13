307e1d464358ef2c87cf9e158e15f869b89c3d9f
Marius Widmann <marius.widmann@gmail.com>
2020-11-13 16:30:07 +0000

    Closes #146

--- it_management/it_management/doctype/subnet/subnet.py
+++ it_management/it_management/doctype/subnet/subnet.py
@@ -5,11 +5,6 @@ from frappe.model.document import Document
 
 import re
 
-def validateIP(ipstr,fieldname):
-	fieldname = fieldname.title()
-	
-
-
 class Subnet(Document):
 	def before_save(self):
 		#Validate Subnet
@@ -18,13 +13,5 @@ class Subnet(Document):
 			if not match:
 				fex = frappe.exceptions.ValidationError("Subnet does not conform to CIDR (e.g. 192.168.1.0/24")
 				frappe.throw(title='Validation',msg='Subnet does not conform to CIDR (e.g. 192.168.1.0/24',exc=fex)
-
-        #Validate WAN Address
-		if self.wan_address:
-			match = re.match(r'(\d{0,3}\.){3}\d{0,3}$',self.wan_address)
-			if not match:
-				msg = "IP Address of field '{fieldname}' does not conform to IP-Notation (e.g. 192.168.1.0)".format(fieldname="WAN Address")
-				fex = frappe.exceptions.ValidationError(msg)
-				frappe.throw(title='Validation',msg=msg,exc=fex)
 
 	pass
