092162e1a35b045e7404e96f15de6521698cf1c5
Marius Widmann <marius.widmann@gmail.com>
2020-12-07 16:00:57 +0000

    Revert "Software Solution: Automatik Generate Table mistake"
    This reverts commit 804db1b2cf0c4fb9854b5a77aef6d63a66fa6010.

--- it_management/it_management/doctype/software_solution/software_solution.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, IT-Geräte und IT-Lösungen wie Server, Rechner, Netzwerke und E-Mailserver sowie auch Backups, and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Software Solution', {
-	// refresh: function(frm) {
-
-	// }
-});
