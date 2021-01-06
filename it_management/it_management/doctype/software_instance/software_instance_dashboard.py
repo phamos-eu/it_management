9233d8798839afca8b0ea0e71743506c5131e491
Wolfram Schmidt <wolfram.schmidt@tueit.de>
2021-01-06 14:08:38 +0100

    Update software_instance_dashboard.py

--- it_management/it_management/doctype/software_instance/software_instance_dashboard.py
+++ it_management/it_management/doctype/software_instance/software_instance_dashboard.py
@@ -6,7 +6,7 @@ def get_data():
     return {
         'fieldname': 'software_instance',
         'non_standard_fieldnames': {
-		'Issue': 'dynamic_name'
+		'Issue': 'dynamic_name',
 		'Task': 'dynamic_name'
         },
         'transactions': [
