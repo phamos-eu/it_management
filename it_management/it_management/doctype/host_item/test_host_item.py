f3e367abd5c25fe9e7497cc53cad7fddf5e8b9d7
Jide Olayinka <olajamesjide@gmail.com>
2024-01-19 10:57:34 +0100

    New doctype Host Item creation

---
+++ it_management/it_management/doctype/host_item/test_host_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, IT-Geräte und IT-Lösungen wie Server, Rechner, Netzwerke und E-Mailserver sowie auch Backups, and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestHostItem(FrappeTestCase):
+	pass
