027eb2efa05b70ad2918fb00578e12ab7c0e2091
Jide Olayinka <olajamesjide@gmail.com>
2024-02-19 16:12:25 +0100

    Created doctype ITM License

---
+++ it_management/it_management/doctype/itm_licence/itm_licence.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, IT-Geräte und IT-Lösungen wie Server, Rechner, Netzwerke und E-Mailserver sowie auch Backups, and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class ITMLicence(Document):
+	pass
